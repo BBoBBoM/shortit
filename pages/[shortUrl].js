4f88b72d6c0459ea11d2ed2300f71730ebf316f7
BBoBBoM <tkrhkakt12@gmail.com>
2024-01-20 11:32:04 +0900

    shorit

---
+++ pages/[shortUrl].js
@@ -0,0 +1,18 @@
+import dbConnect from "@/db/dbConnect";
+import ShortLink from "@/db/models/ShortLink";
+
+export async function getServerSideProps(context) {
+  const { shortUrl } = context.query;
+  await dbConnect();
+  const shortLink = await ShortLink.findOneAndDelete({ shortUrl });
+  if (shortLink) {
+    return {
+      redirect: { destination: shortLink.url, permanent: false },
+    };
+  }
+  return { notFound: true };
+}
+
+export default function ShortUrlPage() {
+  return null;
+}
